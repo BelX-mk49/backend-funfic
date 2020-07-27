f31393a1040fb261a93068032e4f82e665cbf4ee
Yury Bialkevich <darkerjura@gmail.com>
2020-07-27 11:10:17 +0300

    add cors filter

--- src/main/java/by/itransition/backend/BackendFanficApplication.java
+++ src/main/java/by/itransition/backend/BackendFanficApplication.java
@@ -2,7 +2,11 @@ package by.itransition.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @EnableAsync
 @SpringBootApplication
@@ -10,4 +14,22 @@ public class BackendFanficApplication {
     public static void main(String[] args) {
         SpringApplication.run(by.itransition.backend.BackendFanficApplication.class, args);
     }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
 }
+
+
