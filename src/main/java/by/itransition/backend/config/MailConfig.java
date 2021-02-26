ca59269e9ffb4438ba4b50ef90a00cd2b4af1bcb
Yury Bialkevich <darkerjura@gmail.com>
2021-02-26 14:14:58 +0300

    change db properties

--- src/main/java/by/itransition/backend/config/MailConfig.java
+++ src/main/java/by/itransition/backend/config/MailConfig.java
@@ -1,6 +1,5 @@
 package by.itransition.backend.config;
 
-import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
